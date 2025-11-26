c3a75c0b93f58348a608c4b6b739e1789a584474
burntarrow <65312004+burntarrow@users.noreply.github.com>
2025-11-26 13:54:25 -0600

    Update import statement for TableOfContentsModule

--- src/builder.tsx
+++ src/builder.tsx
@@ -3,8 +3,9 @@
  * The module definition mirrors the structure used in the Divi 5 example modules.
  */
 import { registerModule } from './divi-module-shim';
-import TableOfContentsModule from './components/table-of-contents-module';
+import { TableOfContentsModule } from './components/table-of-contents-module';
 import './components/table-of-contents-module/style.scss';
 import './components/table-of-contents-module/module.scss';
 
+// Use the slug from module metadata to register with Divi 5
 registerModule(TableOfContentsModule.metadata.slug, TableOfContentsModule);
